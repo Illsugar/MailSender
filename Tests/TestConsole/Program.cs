b1afb4ebfeff5e5a8e475760cd3581b24e8126aa
Ирина Мин <irinasamylovskaya@mail.ru>
2021-06-15 20:37:24 +0300

    Базовая логика отправки почты

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail; //пространтво имён для работы с почтой 
 
 namespace TestConsole
 {
@@ -6,7 +8,16 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var massege = new MailMessage("irinasamylovskaya@mail.ru", "super.zebra-king@yandex.ru"); //сообщение от кого и кому
+
+            using var client = new SmtpClient("smtp.mail.ru", 465); //для клиента указываем адрес сервера и порт
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "irinasamylovskaya",
+                Password = "123"
+            };
+
+            client.Send(massege);
         }
     }
 }
