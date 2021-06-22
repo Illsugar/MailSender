78f13c65ffa64a375b1ccd9f2f4f1d740f5d4d07
Ирина Мин <irinasamylovskaya@mail.ru>
2021-06-22 20:48:38 +0300

    Создание контрола для вывода данных по получателям

--- MailSender/View/RecipientsView.xaml.cs
+++ MailSender/View/RecipientsView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailSender.Views
+namespace MailSender.View
 {
     /// <summary>
     /// Логика взаимодействия для RecipientsView.xaml
