2b5db953b5ca58ab6e1d8c44f641ad4ceb3d5536
Ирина Мин <irinasamylovskaya@mail.ru>
2021-06-22 14:30:10 +0300

    Добавление репозиториев для отправителей, получателей, писем

---
+++ MailSender/Servicies/InMemory/InMemoryLetterRepository.cs
@@ -0,0 +1,33 @@
+﻿using MailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Servicies.InMemory
+{
+    class InMemoryLetterRepository : InMemoryRepository<Letter>
+    {
+        private static IEnumerable<Letter> GetTestData(int Count = 100) => Enumerable.Range(1, Count)
+           .Select(i => new Letter
+           {
+               Id = i,
+               Title = $"Сообщение {i}",
+               Body = $"Текст сообщения {i}",
+           });
+
+        public InMemoryLetterRepository() : base(GetTestData()) { }
+
+        public override void Update(Letter item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+
+            var db_item = GetById(item.Id);
+            if (db_item is null || ReferenceEquals(db_item, item)) return;
+
+            db_item.Title = item.Title;
+            db_item.Body = item.Body;
+        }
+    }
+}
