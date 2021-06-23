42c7df56eb85c18c37ac495725111858c8a72bd6
Ирина Мин <irinasamylovskaya@mail.ru>
2021-06-23 22:04:40 +0300

    Класс кодирования строки шифром Цезаря

---
+++ MailSender.lib/Services/TextEncoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Services
+{
+    public static class TextEncoder
+    {
+        public static string Encode(string str, int Key = 3)
+        {
+            return new(str.Select(c => (char)(c + Key)).ToArray());
+        }
+
+        public static string Decode(string str, int Key = 3)
+        {
+            return new(str.Select(c => (char)(c - Key)).ToArray());
+        }
+    }
+}
