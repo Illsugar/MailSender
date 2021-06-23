9fb79c28a32b6bc0ee7976f17f0d3e0ad144fd45
Ирина Мин <irinasamylovskaya@mail.ru>
2021-06-23 21:09:55 +0300

    Реагирование на ошибку валидации #3 (с помощью интерфейса)

--- MailSender.lib/Models/Recipient.cs
+++ MailSender.lib/Models/Recipient.cs
@@ -1,22 +1,45 @@
 ﻿using MailSender.Models.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MailSender.Models
 {
-    public class Recipient : Person
+    public class Recipient : Person, IDataErrorInfo
     {
-        public override string Name 
-        { 
-            get => base.Name;
-            set 
+        
+        //public override string Name 
+        //{ 
+        //    get => base.Name;
+        //    set 
+        //    {
+        //        if (string.IsNullOrEmpty(value))
+        //            throw new ArgumentException("Задана пустая строка имени", nameof(value));
+        //        base.Name = value;
+        //    }
+        //}
+
+        string IDataErrorInfo.Error => null;
+
+        string IDataErrorInfo.this[string PropertyName]
+        {
+            get
             {
-                if (string.IsNullOrEmpty(value))
-                    throw new ArgumentException("Задана пустая строка имени", nameof(value));
-                base.Name = value;
+                switch (PropertyName)
+                {
+                    default: return null;
+
+                    case nameof(Name):
+                        var name = Name;
+
+                        if (name is null) return "Строка имени не определена";
+                        if (name == "") return "Строка имени пуста";
+                        if (name.Length < 3) return "Имя должно быть не менее 3 символов";
+                        return null;
+                }
             }
         }
     }
