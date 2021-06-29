45f0eeb6ecc46ceab60261a79e6ba575d82471fc
Ирина Мин <irinasamylovskaya@mail.ru>
2021-06-29 17:21:24 +0300

    Работа с Task и TPL, а также с асинхронными процессами в приложении с UI

--- Tests/TestWpf/MainWindow.xaml.cs
+++ Tests/TestWpf/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
-
+using System.Windows.Controls;
 
 namespace TestWpf
 {
@@ -12,5 +14,85 @@ namespace TestWpf
         {
             InitializeComponent();
         }
+
+        private void Cancel_OnClick(object Sender, RoutedEventArgs E)
+        {
+            _Cancellation?.Cancel();
+        }
+
+        private CancellationTokenSource _Cancellation;
+        private async void Start_OnClick(object Sender, RoutedEventArgs E)
+        {
+            var button = (Button)Sender;
+            button.IsEnabled = false;
+            CancelButton.IsEnabled = true;
+
+            var cancellation = new CancellationTokenSource();
+            _Cancellation = cancellation;
+
+            var progress = new Progress<double>(p => Progress.Value = p * 100);
+            try
+            {
+                var result = await CalculateAsync(Progress: progress, Cancel: cancellation.Token);
+                Result.Text = result.ToString();
+            }
+            catch (OperationCanceledException)
+            {
+                Result.Text = "Отмена!";
+                Progress.Value = 0;
+            }
+
+            button.IsEnabled = true;
+            CancelButton.IsEnabled = false;
+        }
+
+        private static async Task<int> CalculateAsync(int Max = 100, int Timeout = 100, IProgress<double> Progress = null, CancellationToken Cancel = default)
+        {
+            //var thread_id = Thread.CurrentThread.ManagedThreadId;
+
+            await Task.Delay(Timeout, Cancel).ConfigureAwait(false);
+
+            //var thread2_id = Thread.CurrentThread.ManagedThreadId;
+
+            var s = 0;
+            for (var i = 1; i <= Max && !Cancel.IsCancellationRequested; i++)
+            {
+                s += i;
+                Progress?.Report((double)i / Max);
+                await Task.Delay(Timeout, Cancel).ConfigureAwait(false);
+            }
+            Cancel.ThrowIfCancellationRequested();
+
+            Progress?.Report(1);
+            return s;
+        }
+
+        private void Calculate(int Max = 100, int Timeout = 100)
+        {
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+
+            var dispatcher = Application.Current.Dispatcher;
+
+            var s = 0;
+            for (var i = 1; i <= Max; i++)
+            {
+                //Application.Current.Dispatcher.BeginInvoke(new Action(
+                dispatcher.BeginInvoke(new Action(
+                    () =>
+                    {
+                        Progress.Value = i;
+                    }));
+
+                s += i;
+                Thread.Sleep(Timeout);
+            }
+
+            //Result.Dispatcher.Invoke(
+            dispatcher.Invoke(
+                () =>
+                {
+                    Result.Text = s.ToString();
+                });
+        }
+        }
     }
-}
